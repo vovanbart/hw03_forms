87c6067abcba91a7a56672337e7dcdc64c955a7e
vovanbart <vovanbart@icloud.com>
2021-09-08 11:22:47 +0300

    project

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
-# LOGOUT_REDIRECT_URL = 'posts:index'  
+# LOGOUT_REDIRECT_URL = 'posts:index'
 
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
